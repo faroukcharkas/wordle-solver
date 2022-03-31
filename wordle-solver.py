c2ba26ee8120f22908ad9366096bd875e7f21678
faroukc <farouk.charkas@gmail.com>
2022-03-31 11:41:35 -0400

    added examples

--- wordle-solver.py
+++ wordle-solver.py
@@ -7,5 +7,10 @@ from modules.lab import Lab
 from modules.wordle_data import WHITE_BOX, WORDLE_LIST, WORDLE_DICTIONARY
 import gc
 
-wordle_lab: Lab = Lab(iterations=100, first_word='adieu', second_word='nymph')
-wordle_lab.run()
+# LAB EXAMPLE
+# wordle_lab: Lab = Lab(iterations=25, first_word='adieu', second_word='lucky')
+# wordle_lab.run()
+
+# GAME EXAMPLE
+# wordle_game: Game = Game(RoboPlayer(first_word='irate', second_word='nymph'))
+# wordle_game.start()
